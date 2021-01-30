1efbf6cb29fe47630235f026a9879bd3105f9fe7
Nomentsafidy <mnomentsafidy@gmail.com>
2021-01-30 12:45:31 +0300

    feat index/personalController: init all personnal API

--- src/controllers/personal.controller.ts
+++ src/controllers/personal.controller.ts
@@ -1,9 +1,56 @@
 import { Request, Response } from 'express';
 
 export default class PersonalController {
-  public get(_request: Request, response: Response) {  
-    response.send("API work");
+
+  /**
+   * get all personnal
+   * @param _request 
+   * @param response 
+   */
+  public get = (_request: Request, response: Response) => {  
+    response.send("API work...");
   }
+
+  /**
+   * get personnal by mail
+   * @param request 
+   * @param response 
+   */
+  public getByEmail = (request: Request, response: Response) => {  
+    console.log(request.params);    
+    response.send(request.params);
+  }
+
+  /**
+   * add personnal
+   * @param request 
+   * @param response 
+   */
+  public add = (request: Request, response: Response) => {  
+    console.log(request.body);    
+    response.send(request.body);
+  }
+
+  /**
+   * update personnal
+   * @param request 
+   * @param response 
+   */
+  public update = (request: Request, response: Response) => {  
+    console.log(request.body);    
+    response.send(request.body);
+  }
+  
+  /**
+   * delete personnal by email
+   * @param request 
+   * @param response 
+   */
+  public delete = (request: Request, response: Response) => {  
+    console.log(request.params);    
+    response.send(request.params);
+  }
+
 }
 
 export const personalController = new PersonalController();
--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,13 @@ const PORT = 3000;
 
 const app: Application = express();
 
-app.get('/', personalController.get)
+app.use(express.json());
+
+app.get('/personal', personalController.get)
+app.get('/personal/getbyemail/:email', personalController.getByEmail)
+app.get('/personal/delete/:email', personalController.delete);
+app.delete('/personal/delete/:email', personalController.delete);
+app.post('/personal/add', personalController.add);
+app.post('/personal/update', personalController.update);
 
 app.listen(PORT, () => console.log(`server runing on port ${PORT}`))
